19c50ed3173fc7169cec86c7940ac70242c9db83
Iryna Pec <irapets777@gmail.com>
2022-12-04 14:36:42 +0200

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+// let slider = $(document).ready(function () {
+//   $('.your-class').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     dots: true,
+//     infinite: true
+//   });
+// });
+
+$('.item').on('click', function() {
+  $('.item').removeClass('active');
+  $(this).addClass('active');
+  let tabs = $ (this) .data( "tabs");
+
+  $('.content').addClass('d-none');
+  $('.content[data-tabs = ' + tabs + ']').removeClass('d-none');
+  $('.content[data-tabs = ' + tabs + ']').addClass('d-block');
+
+})
 
